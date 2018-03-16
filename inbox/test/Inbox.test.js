e6339dd8afbdd38dda461eed601900720b24aca7
savvala <savva.l@hotmail.com>
2018-03-16 15:44:47 +0000

    default message test

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -3,7 +3,10 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider()); // creates an instance of web3 from Constructor Web3
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
 const { interface, bytecode } = require('../compile');
 
 let accounts;
@@ -17,10 +20,17 @@ beforeEach(async () => {
   inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode, arguments: ['Hi']})
     .send({ from:accounts[0], gas:'1000000' })
+
+    inbox.setProvider(provider);
 });
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
-    console.log(inbox);
+    assert.ok(inbox.options.address);
+  });
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi');
   });
 });
